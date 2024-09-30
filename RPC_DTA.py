53d7d2955bc1f99cc20addedb02b9064bb7abd9d
mdinteligente <125225578+mdinteligente@users.noreply.github.com>
2024-09-30 14:12:15 -0500

    Update RPC_DTA.py

--- RPC_DTA.py
+++ RPC_DTA.py
@@ -126,7 +126,7 @@ st.write(f"Puntuación HEART: {heart_score} (Riesgo de MACE: {riesgo_mace_heart(
 st.write(f"Puntuación HEAR: {hear_score} (Riesgo de MACE: {riesgo_mace_hear(hear_score)})")
 st.write(f"Puntuación EDACS: {edacs_score} (Riesgo de MACE: {riesgo_mace_edacs(edacs_score)})")
 
-st.header("Cálculo de riesgo de MACE y valores diagnósticos")
+st.header("Cocientes de probabilidad y efectos absolutos de aplicar las escalas")
 prevalencia = 0.15
 total_pacientes = 1000
 
